133228a919a36be8421bad873bdabcb6d4e4c327
John Hamlin <johnhamlin@gmail.com>
2022-12-02 16:30:49 -0500

    Delete days already completed

--- src/2022/1/1.ts
+++ src/2022/1/1.ts
@@ -3,18 +3,6 @@ import fs from 'fs';
 const input = fs.readFileSync('input.txt', 'utf8');
 const inputArray = input.split('\n');
 
-let mostCalories = 0;
-let currentCount = 0;
-
 inputArray.forEach((line) => {
-	if (line == '') {
-		if (currentCount > mostCalories) {
-			mostCalories = currentCount;
-		}
-		currentCount = 0;
-	} else {
-		currentCount += Number(line);
-	}
+	// Do your magic here with each line.
 });
-
-console.log(mostCalories);
